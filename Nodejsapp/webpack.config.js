c517c19e03127599d2f2435bd39d50625d4db17c
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2024-07-19 21:15:37 +0530

    Update webpack.config.js

--- Nodejsapp/webpack.config.js
+++ Nodejsapp/webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // or 'production'
-  entry: './src/index.js', // Entry point for your application
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js', // Output filename
-    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    fallback: {
+      url: require.resolve('url/') // Polyfill for 'url'
+    }
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Apply to JavaScript files
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', // Use Babel to transpile JavaScript
-        },
-      },
-      // Add other loaders if needed
-    ],
-  },
-  resolve: {
-    extensions: ['.js'], // File extensions to resolve
-  },
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
